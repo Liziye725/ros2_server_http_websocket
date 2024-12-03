57cb140099050a18f717aaea0a4408e59b0b6033
Ziye Li <144114689+Liziye725@users.noreply.github.com>
2024-12-03 15:59:54 +0100

    Update rectangle_nav_launch.py

--- frontend/turtlebot_rectangle_nav/launch/rectangle_nav_launch.py
+++ frontend/turtlebot_rectangle_nav/launch/rectangle_nav_launch.py
@@ -1,13 +1,22 @@
 import launch
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
 
 def generate_launch_description():
     return LaunchDescription([
+        # Launch the ROS 2 node
         Node(
-            package='turtlebot_rectangle_nav',
-            executable='main.py',
+            package='turtlebot_rectangle_nav',  # ROS package containing robot_nav_node
+            executable='robot_nav_node',        # The executable for the ROS node
             name='robot_nav_node',
-            output='screen'
-        )
+            output='screen',
+        ),
+        
+        # Launch FastAPI server in a separate process (running main.py)
+        ExecuteProcess(
+            cmd=['python3', '../src/main.py'],  # Specify the path to your main.py file
+            name='fastapi_server',
+            output='screen',
+        ),
     ])
